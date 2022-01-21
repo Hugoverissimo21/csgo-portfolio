150f26ed25b9f63046dddb98aa2f6ad2e564db56
Hugoverissimo21 <hugoverissimo21@gmail.com>
2022-01-21 20:09:11 +0000

    finished py tools

--- requests.py
+++ requests.py
@@ -2,9 +2,12 @@ import tools #local file -> tools.py
 
 print("O que queres ver?")
 print("""As opções são:
-1 - Valor investido (from db.csv)""")
+1 - Valor investido (from db.csv)
+2 - Quantidade de items de um tipo (from db.csv)""")
 escolha = input()
 if escolha=="1":
     print("Atualmente, estão investidos",tools.total_invested("db.csv"),"¥")
+elif escolha=="2":
+    print("Atualmente, tens",tools.qnt_de_("db.csv"))
 else:
     print("Opção inválida.")
--- tools.py
+++ tools.py
@@ -11,11 +11,12 @@ def total_invested(file):
         total_spent += float(value)
     return(total_spent)
 
-def qnt_de_(file): ##### por acabar
+def qnt_de_(file):
     csv_lines = csv.reader(open(file,"r"))
+    csv_lines2 = csv.reader(open(file,"r"))
     item_list = []
-    for line in csv_lines:
-        item_list.append(line[2])
+    for [qty , name , type , spent, purchased , notes_info] in csv_lines:
+        item_list.append(type)
     item_list.remove(item_list[0])
     procura = input("Queres ver o quê? ")
     if procura in item_list:
@@ -24,10 +25,19 @@ def qnt_de_(file): ##### por acabar
         for item in item_list:
             if item == procura:
                 item_index.append(item_index_id)
-            intem_index_id += 1
-        return(item_index_id)
+            item_index_id += 1
+        line = int("-1")
+        quantidades = 0
+        for a in csv_lines2:
+            line +=1
+            if line in item_index:
+                quantidades += int(a[0])
+        return(str(quantidades) + " itens / " + str(item_index_id) + " tipos")
     else:
         return("Item não encontrado")
+
+
+
 
 if __name__ == "__main__":
     print(total_invested("db.csv"),"from f(total_invested)")
